e5c56a269dd91c1ba46736819faa9a20fd13f415
SDM0 <60699143+SDM0@users.noreply.github.com>
2025-04-30 23:48:15 +0200

    "Medusa" and "Patchwork Joker"

--- localization/default.lua
+++ localization/default.lua
@@ -1,13 +1,29 @@
 return {
     descriptions = {
         Joker = {
-            j_mxfj_test = {
-                name = "Test",
+            j_mxfj_medusa = {
+                name = "Medusa",
                 text = {
-                    "{C:mult}+#1#{} Mult",
+                    "All played {C:attention}face{} cards",
+                    "become {C:attention}Stone{} cards",
+                    "when scored",
                 }
             },
-
+            j_mxfj_patchwork = {
+                name = "Patchwork Joker",
+                text = {
+                "{C:attention}Patch{} played cards and",
+                "gains half of the rank",
+                "as {C:chips}Chips{} if not {C:attention}patched{}",
+                "{C:inactive}(Currently {C:chips}+#1# {C:inactive}Chips)"
+                }
+            }
         },
     },
+    misc = {
+        dictionary = {
+            k_stone = "Stone",
+            k_patched = "Patched"
+        }
+    }
 }
--- main.lua
+++ main.lua
@@ -30,27 +30,88 @@ mxfj_config = mxfj_mod.config
 --    }}
 --end
 
-SMODS.Atlas{
+SMODS.load_file("utils.lua")()
+
+SMODS.Atlas {
     key = "mxfj_sprites",
     path = "mxfj_sprites.png",
     px = 71,
     py = 95
 }
 
-SMODS.Joker{
-    key = "test",
-    name = "Test",
-    rarity = 1,
-    blueprint_compat = true,
-    pos = {x = 1, y = 0},
-    cost = 2,
-    config = {extra = 4},
+-- Medusa --
+
+SMODS.Joker {
+    key = "medusa",
+    name = "Medusa",
+    rarity = 2,
+    pos = { x = 0, y = 0 },
+    cost = 6,
     loc_vars = function(self, info_queue, card)
-        return {vars = {card.ability.extra}}
+        info_queue[#info_queue + 1] = G.P_CENTERS.m_stone
     end,
     calculate = function(self, card, context)
-        if context.joker_main then
-            return card.ability.extra
+        if context.cardarea == G.jokers and context.before and no_bp_retrigger(context) then
+            local faces = {}
+            for _, v in ipairs(context.scoring_hand) do
+                if v:is_face() then
+                    faces[#faces + 1] = v
+                    v:set_ability(G.P_CENTERS.m_stone, nil, true)
+                    G.E_MANAGER:add_event(Event({
+                        func = function()
+                            v:juice_up()
+                            return true
+                        end
+                    }))
+                end
+            end
+            if #faces > 0 then
+                return {
+                    message = localize('k_stone'),
+                    colour = G.C.GREY,
+                }
+            end
+        end
+    end,
+    atlas = "mxfj_sprites"
+}
+
+-- Patchwork Joker --
+
+SMODS.Joker {
+    key = "patchwork",
+    name = "Patchwork Joker",
+    rarity = 2,
+    pos = { x = 6, y = 0 },
+    cost = 6,
+    config = { extra = 0 },
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra } }
+    end,
+    calculate = function(self, card, context)
+        if context.cardarea == G.play and context.individual and context.other_card and not context.end_of_round and no_bp_retrigger(context) then
+            if context.other_card.debuff then
+                return {
+                    message = localize('k_debuffed'),
+                    colour = G.C.RED,
+                }
+            end
+            if not context.other_card.mxfj_patchwork then
+                context.other_card.mxfj_patchwork = true
+                local _chips = math.floor(context.other_card.base.nominal / 2)
+                card.ability.extra = card.ability.extra + _chips
+                return {
+                    func = function()
+                        card_eval_status_text(context.other_card, 'extra', nil, nil, nil, { message = localize("k_patched") })
+                        card_eval_status_text(card, 'extra', nil, nil, nil, { message = localize { type = 'variable', key = 'a_chips', vars = { _chips } }, colour = G.C.CHIPS })
+                    end
+                }
+            end
+        end
+        if context.joker_main and card.ability.extra ~= 0 then
+            return {
+                chips = card.ability.extra
+            }
         end
     end,
     atlas = "mxfj_sprites"
