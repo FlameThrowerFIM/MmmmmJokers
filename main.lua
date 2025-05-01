9e475ebbf8d246cfc718890befe5664a3116dd5c
SDM0 <60699143+SDM0@users.noreply.github.com>
2025-05-01 15:51:12 +0200

    Updated "Patchwork Joker"

--- main.lua
+++ main.lua
@@ -78,6 +78,26 @@ SMODS.Joker {
 
 -- Patchwork Joker --
 
+SMODS.Atlas{
+    key = "mxfj_patch",
+    path = "mxfj_patch.png",
+    px = 71,
+    py = 95
+}
+
+SMODS.DrawStep{
+    key = 'mxfj_patch',
+    order = -1,
+    func = function(self)
+        if self.ability and self.ability.mxfj_patchwork then
+            if not mxfj_mod.mxfj_patch then mxfj_mod.mxfj_patch = Sprite(0, 0, G.CARD_W, G.CARD_H, G.ASSET_ATLAS["mxfj_patch"], {x = 0,y = 0}) end
+            mxfj_mod.mxfj_patch.role.draw_major = self
+            mxfj_mod.mxfj_patch:draw_shader('dissolve', nil, nil, nil, self.children.center)
+        end
+    end,
+    conditions = {vortex = false, facing = 'front'}
+}
+
 SMODS.Joker {
     key = "patchwork",
     name = "Patchwork Joker",
@@ -89,23 +109,24 @@ SMODS.Joker {
         return { vars = { card.ability.extra } }
     end,
     calculate = function(self, card, context)
-        if context.cardarea == G.play and context.individual and context.other_card and not context.end_of_round and no_bp_retrigger(context) then
+        if context.cardarea == G.play and context.individual and not context.end_of_round and no_bp_retrigger(context) then
             if context.other_card.debuff then
                 return {
                     message = localize('k_debuffed'),
                     colour = G.C.RED,
                 }
             end
-            if not context.other_card.mxfj_patchwork then
-                context.other_card.mxfj_patchwork = true
+            if not context.other_card.ability.mxfj_patchwork then
                 local _chips = math.ceil(context.other_card.base.nominal / 2)
                 card.ability.extra = card.ability.extra + _chips
-                return {
+                local _card = context.other_card
+                G.E_MANAGER:add_event(Event({
                     func = function()
-                        card_eval_status_text(context.other_card, 'extra', nil, nil, nil, { message = localize("k_patched") })
-                        card_eval_status_text(card, 'extra', nil, nil, nil, { message = localize { type = 'variable', key = 'a_chips', vars = { _chips } }, colour = G.C.CHIPS })
-                    end
-                }
+                        _card.ability.mxfj_patchwork = true
+                    return true
+                end}))
+                card_eval_status_text(context.other_card, 'extra', nil, nil, nil, { message = localize("k_patched") })
+                card_eval_status_text(card, 'extra', nil, nil, nil, { message = localize { type = 'variable', key = 'a_chips', vars = { _chips } }, colour = G.C.CHIPS })
             end
         end
         if context.joker_main and card.ability.extra ~= 0 then
