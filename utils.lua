83a351888211481707600e3c4026da8326bc7497
SDM0 <60699143+SDM0@users.noreply.github.com>
2025-05-02 22:09:06 +0200

    Added util to count "Cultist" cards

--- utils.lua
+++ utils.lua
@@ -17,6 +17,21 @@ function no_bp_retrigger(context)
     return not (context.blueprint or context.retrigger_joker or context.retrigger_joker_check)
 end
 
+--- Counts how many Carcinization there is
+function get_cultist_count()
+    local cultist_count = 0
+    if G.jokers and G.jokers.cards then
+        if #G.jokers.cards > 0 then
+            for i = 1, #G.jokers.cards do
+                if G.jokers.cards[i].config.center.key == "j_mxfj_cultist" and not G.jokers.cards[i].debuff then
+                    cultist_count = cultist_count + 1
+                end
+            end
+        end
+    end
+    return cultist_count
+end
+
 --- Talisman compat
 to_big = to_big or function(num)
     return num
